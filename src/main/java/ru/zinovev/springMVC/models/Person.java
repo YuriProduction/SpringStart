a1b2406c89e1cfbed2a65251e723e9d3827ed93d
Jurex <jurexda@yandex.ru>
2023-09-19 12:17:11 +0500

    Added validation but does not work :(((((((((((((((((

--- src/main/java/ru/zinovev/springMVC/models/Person.java
+++ src/main/java/ru/zinovev/springMVC/models/Person.java
@@ -1,30 +1,34 @@
 package ru.zinovev.springMVC.models;
 
-/**
- * @author Neil Alishev
- */
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
 
-    public String getSurname() {
-        return surname;
-    }
+    @Min(value = 0, message = "Age should be greater than 0")
+    private int age;
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    private String surname;
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
 
     public Person() {
 
     }
 
-    public Person(int id, String name, String surname) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
-        this.surname = surname;
+        this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -41,5 +45,21 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
